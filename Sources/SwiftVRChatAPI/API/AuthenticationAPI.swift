5a82f268111383e07d28c9c96c7e3310bf69799a
Tongguang Watanabe Xia <ghbq674521@163.com>
2023-05-22 15:23:34 -0500

    feat(auth): async support

--- Sources/SwiftVRChatAPI/API/AuthenticationAPI.swift
+++ Sources/SwiftVRChatAPI/API/AuthenticationAPI.swift
@@ -11,10 +11,73 @@ import Foundation
 // MARK: Authentication API
 //
 
-public struct AuthenticationAPI {
-    static let authUrl = "\(baseUrl)/auth"
-    static let auth2FAUrl = "\(baseUrl)/auth/twofactorauth"
+let authUrl = "\(baseUrl)/auth"
+let auth2FAUrl = "\(baseUrl)/auth/twofactorauth"
+
+struct VerifyResponse: Codable {
+    let verified: Bool
+}
+
+@available(iOS 15.0, *)
+public struct AuthenticationAPIAsync {
     
+    public static func loginUserInfo(client: APIClientAsync) async -> User? {
+        let url = URL(string: "\(authUrl)/user")!
+        
+        let (data, _) = await client.VRChatRequest(url: url,
+                             httpMethod: "GET",
+                             authorization: true,
+                             auth: true,
+                             twoFactorAuth: true)
+        
+        guard let data = data else { return nil }
+
+        let user:User? = decode(data: data)
+
+        client.updateCookies()
+        
+        return user
+    }
+    
+    public static func verify2FAEmail(client: APIClientAsync, emailOTP: String) async -> Bool? {
+        let url = URL(string: "\(auth2FAUrl)/emailotp/verify")!
+        
+        let httpBody: Data?
+        do {
+            httpBody = try JSONSerialization.data(withJSONObject: ["code" : emailOTP], options: .prettyPrinted)
+        } catch let error {
+            print(error.localizedDescription)
+            return nil
+        }
+        
+        let (data, _) = await client.VRChatRequest(url: url,
+                             httpMethod: "POST",
+                             auth: true,
+                             contentType: "application/json",
+                             httpBody: httpBody)
+        
+        guard let data = data else { return nil }
+
+        let verifyResponse:VerifyResponse? = decode(data: data)
+        
+        client.updateCookies()
+        
+        return verifyResponse?.verified
+        
+    }
+    
+    public static func logout(client: APIClientAsync) async {
+        let url = URL(string: "\(baseUrl)/logout")!
+        
+        let (_, _) = await client.VRChatRequest(url: url,
+                             httpMethod: "PUT",
+                             auth: true)
+
+        client.updateCookies()
+    }
+}
+
+public struct AuthenticationAPI {
     public static func loginUserInfo(client: APIClient, completionHandler: @escaping @Sendable (User?) -> Void) {
         let url = URL(string: "\(authUrl)/user")!
         
@@ -45,11 +108,6 @@ public struct AuthenticationAPI {
     }
     
     public static func verify2FAEmail(client: APIClient, emailOTP: String, completionHandler: @escaping @Sendable (Bool?) -> Void) {
-        
-        struct VerifyResponse: Codable {
-            let verified: Bool
-        }
-        
         let url = URL(string: "\(auth2FAUrl)/emailotp/verify")!
         
         let httpBody: Data?
--- Sources/SwiftVRChatAPI/Client.swift
+++ Sources/SwiftVRChatAPI/Client.swift
@@ -11,6 +11,101 @@ import Foundation
 // MARK: API Client
 //
 
+@available(iOS 15.0, *)
+public class APIClientAsync {
+    private var username: String?
+    private var password: String?
+    
+    // Cookies
+    private var auth: String?
+    private var twoFactorAuth: String?
+    private var apiKey: String?
+
+    
+    public init() {}
+    
+    public init(username: String, password: String) {
+        self.username = username
+        self.password = password
+    }
+    
+    public func updateCookies() {
+        self.auth = nil
+        self.twoFactorAuth = nil
+        self.apiKey = nil
+        for cookie in HTTPCookieStorage.shared.cookies(for: URL(string: domainUrl)!)! {
+            if (cookie.name == "auth") {
+                self.auth = cookie.value
+            } else if (cookie.name == "twoFactorAuth") {
+                self.twoFactorAuth = cookie.value
+            } else if (cookie.name == "apiKey") {
+                self.apiKey = cookie.value
+            }
+        }
+        
+        //Debug
+//        print("*** updateCookies() ***")
+//        print("auth: \(auth)")
+//        print("twoFactorAuth: \(twoFactorAuth)")
+//        print("apiKey: \(apiKey)")
+        //Debug End
+    }
+    
+    func VRChatRequest(url: URL,
+                       httpMethod: String,
+                       authorization: Bool = false,
+                       auth: Bool = false, twoFactorAuth: Bool = false, apiKey: Bool = false,
+                       contentType: String? = nil,
+                       httpBody: Data? = nil) async -> (Data?, URLResponse?) {
+        var request = URLRequest(url: url)
+        request.httpMethod = httpMethod
+        
+        /// Authorization
+        if authorization {
+            let authData = ((username ?? "") + ":" + (password ?? "")).data(using: .utf8)!.base64EncodedString()
+            request.addValue("Basic \(authData)", forHTTPHeaderField: "Authorization")
+        }
+        
+        /// Cookie
+        var cookie = ""
+        if auth {
+            cookie.append("auth=\(self.auth ?? "auth"); ")
+//            request.addValue("auth=\(self.auth ?? "auth")", forHTTPHeaderField: "Cookie")
+        }
+        if twoFactorAuth {
+            cookie.append("twoFactorAuth=\(self.twoFactorAuth ?? "twoFactorAuth"); ")
+//            request.addValue("twoFactorAuth=\(self.twoFactorAuth ?? "twoFactorAuth")", forHTTPHeaderField: "Cookie")
+        }
+        if apiKey {
+            cookie.append("apiKey=\(self.apiKey ?? "apiKey"); ")
+//            request.addValue("apiKey=\(self.apiKey ?? "apiKey")", forHTTPHeaderField: "Cookie")
+        }
+        request.addValue(cookie, forHTTPHeaderField: "Cookie")
+        
+        //Debug
+//        print("*** VRChatRequest() ***")
+//        print(request.allHTTPHeaderFields)
+        //Debug End
+        
+        /// HTTP Body
+        if let contentType = contentType, let httpBody = httpBody {
+            request.addValue(contentType, forHTTPHeaderField: "Content-Type")
+            request.httpBody = httpBody
+        }
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            return (data, response)
+        } catch {
+            print(error.localizedDescription)
+            
+            return (nil, nil)
+        }
+    }
+}
+
+
 public class APIClient {
     private var username: String?
     private var password: String?
